72c33e18e2865d1ecea5222912b02314e79e0c91
fareehaahmed2006-byte <fareehaahmed2006@gmail.com>
2025-12-29 20:47:05 +0500

    Updated

--- SECURITY.md
+++ SECURITY.md
@@ -1,24 +1,41 @@
-# Security Policies and Procedures
+<!-- BEGIN MICROSOFT SECURITY.MD V0.0.9 BLOCK -->
 
-## Reporting a Bug
+## Security
 
-The `send` team and community take all security bugs seriously. Thank you
-for improving the security of Express. We appreciate your efforts and
-responsible disclosure and will make every effort to acknowledge your
-contributions.
+Microsoft takes the security of our software products and services seriously, which includes all source code repositories managed through our GitHub organizations, which include [Microsoft](https://github.com/Microsoft), [Azure](https://github.com/Azure), [DotNet](https://github.com/dotnet), [AspNet](https://github.com/aspnet) and [Xamarin](https://github.com/xamarin).
 
-Report security bugs by emailing the current owner(s) of `send`. This information
-can be found in the npm registry using the command `npm owner ls send`.
-If unsure or unable to get the information from the above, open an issue
-in the [project issue tracker](https://github.com/pillarjs/send/issues)
-asking for the current contact information.
+If you believe you have found a security vulnerability in any Microsoft-owned repository that meets [Microsoft's definition of a security vulnerability](https://aka.ms/security.md/definition), please report it to us as described below.
 
-To ensure the timely response to your report, please ensure that the entirety
-of the report is contained within the email body and not solely behind a web
-link or an attachment.
+## Reporting Security Issues
 
-At least one owner will acknowledge your email within 48 hours, and will send a
-more detailed response within 48 hours indicating the next steps in handling
-your report. After the initial reply to your report, the owners will
-endeavor to keep you informed of the progress towards a fix and full
-announcement, and may ask for additional information or guidance.
+**Please do not report security vulnerabilities through public GitHub issues.**
+
+Instead, please report them to the Microsoft Security Response Center (MSRC) at [https://msrc.microsoft.com/create-report](https://aka.ms/security.md/msrc/create-report).
+
+If you prefer to submit without logging in, send email to [secure@microsoft.com](mailto:secure@microsoft.com).  If possible, encrypt your message with our PGP key; please download it from the [Microsoft Security Response Center PGP Key page](https://aka.ms/security.md/msrc/pgp).
+
+You should receive a response within 24 hours. If for some reason you do not, please follow up via email to ensure we received your original message. Additional information can be found at [microsoft.com/msrc](https://www.microsoft.com/msrc). 
+
+Please include the requested information listed below (as much as you can provide) to help us better understand the nature and scope of the possible issue:
+
+  * Type of issue (e.g. buffer overflow, SQL injection, cross-site scripting, etc.)
+  * Full paths of source file(s) related to the manifestation of the issue
+  * The location of the affected source code (tag/branch/commit or direct URL)
+  * Any special configuration required to reproduce the issue
+  * Step-by-step instructions to reproduce the issue
+  * Proof-of-concept or exploit code (if possible)
+  * Impact of the issue, including how an attacker might exploit the issue
+
+This information will help us triage your report more quickly.
+
+If you are reporting for a bug bounty, more complete reports can contribute to a higher bounty award. Please visit our [Microsoft Bug Bounty Program](https://aka.ms/security.md/msrc/bounty) page for more details about our active programs.
+
+## Preferred Languages
+
+We prefer all communications to be in English.
+
+## Policy
+
+Microsoft follows the principle of [Coordinated Vulnerability Disclosure](https://aka.ms/security.md/cvd).
+
+<!-- END MICROSOFT SECURITY.MD BLOCK -->
--- index.d.ts
+++ index.d.ts
@@ -1,332 +1,80 @@
-import { BaseError } from 'make-error';
-import type * as _ts from 'typescript';
-import type { TSCommon } from './ts-compiler-types';
-import type { createEsmHooks as createEsmHooksFn } from './esm';
-export { TSCommon };
-export { createRepl, CreateReplOptions, ReplService, EvalAwarePartialHost, } from './repl';
-export type { TranspilerModule, TranspilerFactory, CreateTranspilerOptions, TranspileOutput, TranspileOptions, Transpiler, } from './transpilers/types';
-export type { NodeLoaderHooksAPI1, NodeLoaderHooksAPI2, NodeLoaderHooksFormat, } from './esm';
-/**
- * Registered `ts-node` instance information.
- */
-export declare const REGISTER_INSTANCE: unique symbol;
-/**
- * Expose `REGISTER_INSTANCE` information on node.js `process`.
- */
-declare global {
-    namespace NodeJS {
-        interface Process {
-            [REGISTER_INSTANCE]?: Service;
-        }
-    }
-}
-/**
- * Export the current version.
- */
-export declare const VERSION: any;
-/**
- * Options for creating a new TypeScript compiler instance.
+import Dispatcher from './dispatcher'
+import { setGlobalDispatcher, getGlobalDispatcher } from './global-dispatcher'
+import { setGlobalOrigin, getGlobalOrigin } from './global-origin'
+import Pool from './pool'
+import { RedirectHandler, DecoratorHandler } from './handlers'
 
- * @category Basic
- */
-export interface CreateOptions {
-    /**
-     * Behave as if invoked within this working directory.  Roughly equivalent to `cd $dir && ts-node ...`
-     *
-     * @default process.cwd()
-     */
-    cwd?: string;
-    /**
-     * Legacy alias for `cwd`
-     *
-     * @deprecated use `projectSearchDir` or `cwd`
-     */
-    dir?: string;
-    /**
-     * Emit output files into `.ts-node` directory.
-     *
-     * @default false
-     */
-    emit?: boolean;
-    /**
-     * Scope compiler to files within `scopeDir`.
-     *
-     * @default false
-     */
-    scope?: boolean;
-    /**
-     * @default First of: `tsconfig.json` "rootDir" if specified, directory containing `tsconfig.json`, or cwd if no `tsconfig.json` is loaded.
-     */
-    scopeDir?: string;
-    /**
-     * Use pretty diagnostic formatter.
-     *
-     * @default false
-     */
-    pretty?: boolean;
-    /**
-     * Use TypeScript's faster `transpileModule`.
-     *
-     * @default false
-     */
-    transpileOnly?: boolean;
-    /**
-     * **DEPRECATED** Specify type-check is enabled (e.g. `transpileOnly == false`).
-     *
-     * @default true
-     */
-    typeCheck?: boolean;
-    /**
-     * Use TypeScript's compiler host API instead of the language service API.
-     *
-     * @default false
-     */
-    compilerHost?: boolean;
-    /**
-     * Logs TypeScript errors to stderr instead of throwing exceptions.
-     *
-     * @default false
-     */
-    logError?: boolean;
-    /**
-     * Load "files" and "include" from `tsconfig.json` on startup.
-     *
-     * Default is to override `tsconfig.json` "files" and "include" to only include the entrypoint script.
-     *
-     * @default false
-     */
-    files?: boolean;
-    /**
-     * Specify a custom TypeScript compiler.
-     *
-     * @default "typescript"
-     */
-    compiler?: string;
-    /**
-     * Specify a custom transpiler for use with transpileOnly
-     */
-    transpiler?: string | [string, object];
-    /**
-     * Transpile with swc instead of the TypeScript compiler, and skip typechecking.
-     *
-     * Equivalent to setting both `transpileOnly: true` and `transpiler: 'ts-node/transpilers/swc'`
-     *
-     * For complete instructions: https://typestrong.org/ts-node/docs/transpilers
-     */
-    swc?: boolean;
-    /**
-     * Paths which should not be compiled.
-     *
-     * Each string in the array is converted to a regular expression via `new RegExp()` and tested against source paths prior to compilation.
-     *
-     * Source paths are normalized to posix-style separators, relative to the directory containing `tsconfig.json` or to cwd if no `tsconfig.json` is loaded.
-     *
-     * Default is to ignore all node_modules subdirectories.
-     *
-     * @default ["(?:^|/)node_modules/"]
-     */
-    ignore?: string[];
-    /**
-     * Path to TypeScript config file or directory containing a `tsconfig.json`.
-     * Similar to the `tsc --project` flag: https://www.typescriptlang.org/docs/handbook/compiler-options.html
-     */
-    project?: string;
-    /**
-     * Search for TypeScript config file (`tsconfig.json`) in this or parent directories.
-     */
-    projectSearchDir?: string;
-    /**
-     * Skip project config resolution and loading.
-     *
-     * @default false
-     */
-    skipProject?: boolean;
-    /**
-     * Skip ignore check, so that compilation will be attempted for all files with matching extensions.
-     *
-     * @default false
-     */
-    skipIgnore?: boolean;
-    /**
-     * JSON object to merge with TypeScript `compilerOptions`.
-     *
-     * @allOf [{"$ref": "https://schemastore.azurewebsites.net/schemas/json/tsconfig.json#definitions/compilerOptionsDefinition/properties/compilerOptions"}]
-     */
-    compilerOptions?: object;
-    /**
-     * Ignore TypeScript warnings by diagnostic code.
-     */
-    ignoreDiagnostics?: Array<number | string>;
-    /**
-     * Modules to require, like node's `--require` flag.
-     *
-     * If specified in `tsconfig.json`, the modules will be resolved relative to the `tsconfig.json` file.
-     *
-     * If specified programmatically, each input string should be pre-resolved to an absolute path for
-     * best results.
-     */
-    require?: Array<string>;
-    readFile?: (path: string) => string | undefined;
-    fileExists?: (path: string) => boolean;
-    transformers?: _ts.CustomTransformers | ((p: _ts.Program) => _ts.CustomTransformers);
-    /**
-     * Allows the usage of top level await in REPL.
-     *
-     * Uses node's implementation which accomplishes this with an AST syntax transformation.
-     *
-     * Enabled by default when tsconfig target is es2018 or above. Set to false to disable.
-     *
-     * **Note**: setting to `true` when tsconfig target is too low will throw an Error.  Leave as `undefined`
-     * to get default, automatic behavior.
-     */
-    experimentalReplAwait?: boolean;
-    /**
-     * Override certain paths to be compiled and executed as CommonJS or ECMAScript modules.
-     * When overridden, the tsconfig "module" and package.json "type" fields are overridden, and
-     * the file extension is ignored.
-     * This is useful if you cannot use .mts, .cts, .mjs, or .cjs file extensions;
-     * it achieves the same effect.
-     *
-     * Each key is a glob pattern following the same rules as tsconfig's "include" array.
-     * When multiple patterns match the same file, the last pattern takes precedence.
-     *
-     * `cjs` overrides matches files to compile and execute as CommonJS.
-     * `esm` overrides matches files to compile and execute as native ECMAScript modules.
-     * `package` overrides either of the above to default behavior, which obeys package.json "type" and
-     * tsconfig.json "module" options.
-     */
-    moduleTypes?: ModuleTypes;
-    /**
-     * A function to collect trace messages from the TypeScript compiler, for example when `traceResolution` is enabled.
-     *
-     * @default console.log
-     */
-    tsTrace?: (str: string) => void;
-    /**
-     * Enable native ESM support.
-     *
-     * For details, see https://typestrong.org/ts-node/docs/imports#native-ecmascript-modules
-     */
-    esm?: boolean;
-    /**
-     * Re-order file extensions so that TypeScript imports are preferred.
-     *
-     * For example, when both `index.js` and `index.ts` exist, enabling this option causes `require('./index')` to resolve to `index.ts` instead of `index.js`
-     *
-     * @default false
-     */
-    preferTsExts?: boolean;
-    /**
-     * Like node's `--experimental-specifier-resolution`, , but can also be set in your `tsconfig.json` for convenience.
-     *
-     * For details, see https://nodejs.org/dist/latest-v18.x/docs/api/esm.html#customizing-esm-specifier-resolution-algorithm
-     */
-    experimentalSpecifierResolution?: 'node' | 'explicit';
-    /**
-     * Allow using voluntary `.ts` file extension in import specifiers.
-     *
-     * Typically, in ESM projects, import specifiers must have an emit extension, `.js`, `.cjs`, or `.mjs`,
-     * and we automatically map to the corresponding `.ts`, `.cts`, or `.mts` source file.  This is the
-     * recommended approach.
-     *
-     * However, if you really want to use `.ts` in import specifiers, and are aware that this may
-     * break tooling, you can enable this flag.
-     */
-    experimentalTsImportSpecifiers?: boolean;
-}
-export declare type ModuleTypes = Record<string, ModuleTypeOverride>;
-export declare type ModuleTypeOverride = 'cjs' | 'esm' | 'package';
-/**
- * Options for registering a TypeScript compiler instance globally.
+import BalancedPool from './balanced-pool'
+import Client from './client'
+import H2CClient from './h2c-client'
+import buildConnector from './connector'
+import errors from './errors'
+import Agent from './agent'
+import MockClient from './mock-client'
+import MockPool from './mock-pool'
+import MockAgent from './mock-agent'
+import { SnapshotAgent } from './snapshot-agent'
+import { MockCallHistory, MockCallHistoryLog } from './mock-call-history'
+import mockErrors from './mock-errors'
+import ProxyAgent from './proxy-agent'
+import EnvHttpProxyAgent from './env-http-proxy-agent'
+import RetryHandler from './retry-handler'
+import RetryAgent from './retry-agent'
+import { request, pipeline, stream, connect, upgrade } from './api'
+import interceptors from './interceptors'
 
- * @category Basic
- */
-export interface RegisterOptions extends CreateOptions {
-    /**
-     * Enable experimental features that re-map imports and require calls to support:
-     * `baseUrl`, `paths`, `rootDirs`, `.js` to `.ts` file extension mappings,
-     * `outDir` to `rootDir` mappings for composite projects and monorepos.
-     *
-     * For details, see https://github.com/TypeStrong/ts-node/issues/1514
-     */
-    experimentalResolver?: boolean;
-}
-export declare type ExperimentalSpecifierResolution = 'node' | 'explicit';
-/**
- * Must be an interface to support `typescript-json-schema`.
- */
-export interface TsConfigOptions extends Omit<RegisterOptions, 'transformers' | 'readFile' | 'fileExists' | 'skipProject' | 'project' | 'dir' | 'cwd' | 'projectSearchDir' | 'optionBasePaths' | 'tsTrace'> {
-}
-/**
- * Information retrieved from type info check.
- */
-export interface TypeInfo {
-    name: string;
-    comment: string;
-}
-/**
- * TypeScript diagnostics error.
- */
-export declare class TSError extends BaseError {
-    diagnosticCodes: number[];
-    name: string;
-    diagnosticText: string;
-    diagnostics: ReadonlyArray<_ts.Diagnostic>;
-    constructor(diagnosticText: string, diagnosticCodes: number[], diagnostics?: ReadonlyArray<_ts.Diagnostic>);
-}
-/**
- * Primary ts-node service, which wraps the TypeScript API and can compile TypeScript to JavaScript
- */
-export interface Service {
-    ts: TSCommon;
-    config: _ts.ParsedCommandLine;
-    options: RegisterOptions;
-    enabled(enabled?: boolean): boolean;
-    ignored(fileName: string): boolean;
-    compile(code: string, fileName: string, lineOffset?: number): string;
-    getTypeInfo(code: string, fileName: string, position: number): TypeInfo;
-}
-/**
- * Re-export of `Service` interface for backwards-compatibility
- * @deprecated use `Service` instead
- * @see {Service}
- */
-export declare type Register = Service;
-/**
- * Create a new TypeScript compiler instance and register it onto node.js
- *
- * @category Basic
- */
-export declare function register(opts?: RegisterOptions): Service;
-/**
- * Register TypeScript compiler instance onto node.js
+export * from './util'
+export * from './cookies'
+export * from './eventsource'
+export * from './fetch'
+export * from './formdata'
+export * from './diagnostics-channel'
+export * from './websocket'
+export * from './content-type'
+export * from './cache'
+export { Interceptable } from './mock-interceptor'
 
- * @category Basic
- */
-export declare function register(service: Service): Service;
-/**
- * Create TypeScript compiler instance.
- *
- * @category Basic
- */
-export declare function create(rawOptions?: CreateOptions): Service;
-/**
- * Create an implementation of node's ESM loader hooks.
- *
- * This may be useful if you
- * want to wrap or compose the loader hooks to add additional functionality or
- * combine with another loader.
- *
- * Node changed the hooks API, so there are two possible APIs.  This function
- * detects your node version and returns the appropriate API.
- *
- * @category ESM Loader
- */
-export declare const createEsmHooks: typeof createEsmHooksFn;
-/**
- * When using `module: nodenext` or `module: node12`, there are two possible styles of emit depending in file extension or package.json "type":
- *
- * - CommonJS with dynamic imports preserved (not transformed into `require()` calls)
- * - ECMAScript modules with `import foo = require()` transformed into `require = createRequire(); const foo = require()`
- */
-export declare type NodeModuleEmitKind = 'nodeesm' | 'nodecjs';
+declare function globalThisInstall (): void
+
+export { Dispatcher, BalancedPool, Pool, Client, buildConnector, errors, Agent, request, stream, pipeline, connect, upgrade, setGlobalDispatcher, getGlobalDispatcher, setGlobalOrigin, getGlobalOrigin, interceptors, MockClient, MockPool, MockAgent, SnapshotAgent, MockCallHistory, MockCallHistoryLog, mockErrors, ProxyAgent, EnvHttpProxyAgent, RedirectHandler, DecoratorHandler, RetryHandler, RetryAgent, H2CClient, globalThisInstall as install }
+export default Undici
+
+declare namespace Undici {
+  const Dispatcher: typeof import('./dispatcher').default
+  const Pool: typeof import('./pool').default
+  const RedirectHandler: typeof import ('./handlers').RedirectHandler
+  const DecoratorHandler: typeof import ('./handlers').DecoratorHandler
+  const RetryHandler: typeof import ('./retry-handler').default
+  const BalancedPool: typeof import('./balanced-pool').default
+  const Client: typeof import('./client').default
+  const H2CClient: typeof import('./h2c-client').default
+  const buildConnector: typeof import('./connector').default
+  const errors: typeof import('./errors').default
+  const Agent: typeof import('./agent').default
+  const setGlobalDispatcher: typeof import('./global-dispatcher').setGlobalDispatcher
+  const getGlobalDispatcher: typeof import('./global-dispatcher').getGlobalDispatcher
+  const request: typeof import('./api').request
+  const stream: typeof import('./api').stream
+  const pipeline: typeof import('./api').pipeline
+  const connect: typeof import('./api').connect
+  const upgrade: typeof import('./api').upgrade
+  const MockClient: typeof import('./mock-client').default
+  const MockPool: typeof import('./mock-pool').default
+  const MockAgent: typeof import('./mock-agent').default
+  const SnapshotAgent: typeof import('./snapshot-agent').SnapshotAgent
+  const MockCallHistory: typeof import('./mock-call-history').MockCallHistory
+  const MockCallHistoryLog: typeof import('./mock-call-history').MockCallHistoryLog
+  const mockErrors: typeof import('./mock-errors').default
+  const fetch: typeof import('./fetch').fetch
+  const Headers: typeof import('./fetch').Headers
+  const Response: typeof import('./fetch').Response
+  const Request: typeof import('./fetch').Request
+  const FormData: typeof import('./formdata').FormData
+  const caches: typeof import('./cache').caches
+  const interceptors: typeof import('./interceptors').default
+  const cacheStores: {
+    MemoryCacheStore: typeof import('./cache-interceptor').default.MemoryCacheStore,
+    SqliteCacheStore: typeof import('./cache-interceptor').default.SqliteCacheStore
+  }
+  const install: typeof globalThisInstall
+}
